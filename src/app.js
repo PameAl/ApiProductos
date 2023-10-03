b7f5e04f894ad21e0a52ec2c4ef2ea69daa10b31
PAMELA ALMADA <pamelaines04@gmail.com>
2023-10-03 11:51:58 -0300

    Varios

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import config from './config'//aqui importamos todo el modulo config
 
 import productsRoutes from './routers/products.routes'
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express()
 
@@ -14,4 +16,5 @@ app.use(express.urlencoded({ extended: false}));
 
 app.use(productsRoutes);
 
+  
 export default app;
--- src/routers/products.routes.js
+++ src/routers/products.routes.js
@@ -12,16 +12,88 @@ import {
 
 const router = Router()
 
+/**
+ * @swagger
+ * /productos:
+ *   get:
+ *     summary: Obtiene todos los productos.
+ *     description: Obtiene una lista de todos los productos disponibles.
+ *     responses:
+ *       200:
+ *         description: Lista de productos obtenida con éxito.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.get('/productos', getProducts)
 
+/**
+ * @swagger
+ * /productos:
+ *   get:
+ *     summary: Carga producto.
+ *     description: Obtiene la carga de un nuevo producto.
+ *     responses:
+ *       200:
+ *         description: Producto cargado con éxito.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.post('/productos', createNewProduct)//para crear productos
 
+/**
+ * @swagger
+ * /productos:
+ *   get:
+ *     summary: Obtiene la cantidad de productos cargados.
+ *     description: Obtiene la cantidad de todos los productos disponibles.
+ *     responses:
+ *       200:
+ *         description: Cantidad de productos obtenidas con éxito.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.get('/productos/count', getTotalProducts)
 
+/**
+ * @swagger
+ * /productos:
+ *   get:
+ *     summary: Obtiene por id el producto indicado.
+ *     description: Obtiene por id el productos disponibles.
+ *     responses:
+ *       200:
+ *         description:  Productos obtenido con éxito.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.get('/productos/:id', getProductById);//para obtener por id
 
+/**
+ * @swagger
+ * /productos:
+ *   get:
+ *     summary: Elimna el producto indicado.
+ *     description: Elimina el producto disponibles.
+ *     responses:
+ *       200:
+ *         description: Producto eliminado con éxito.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.delete('/productos/:id', deleteProductById)//para eliminar
 
+/**
+ * @swagger
+ * /productos:
+ *   get:
+ *     summary: Actualiza el producto indicado.
+ *     description: Actualiza el productos disponibles.
+ *     responses:
+ *       200:
+ *         description: Producto actualizado con éxito.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.put('/productos/:id',  updateProductById)//para actualizar
 
 export default router 
